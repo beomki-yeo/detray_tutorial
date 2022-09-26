5d146786b1d2646751634751e4deb6fd5bf31d32
beomki-yeo <beomki.yeo@gmail.com>
2022-09-26 16:27:08 -0700

    Hello World

---
+++ tutorials/propagation/propagation.cpp
@@ -0,0 +1,12 @@
+/** Detray tutorial project, No copy right **/
+
+// Project include(s).
+#include "propagation.hpp"
+
+#include <iostream>
+
+int main(){
+    std::cout << "Hello World" << std::endl;
+    
+    return 0;
+}
---
+++ tutorials/propagation/propagation.hpp
@@ -0,0 +1,17 @@
+/** Detray tutorial project, No copy right **/
+
+#pragma once
+
+// Detray include(s).
+#include "detray/plugins/algebra/array_definitions.hpp"
+#include "detray/definitions/units.hpp"
+#include "detray/field/constant_magnetic_field.hpp"
+#include "detray/propagator/aborters.hpp"
+#include "detray/propagator/actor_chain.hpp"
+#include "detray/propagator/base_actor.hpp"
+#include "detray/propagator/navigator.hpp"
+#include "detray/propagator/propagator.hpp"
+#include "detray/propagator/rk_stepper.hpp"
+#include "detray/propagator/track.hpp"
+#include "tests/common/tools/create_toy_geometry.hpp"
+#include "tests/common/tools/track_generators.hpp"
